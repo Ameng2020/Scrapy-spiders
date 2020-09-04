b0e4602f6e4350ee02e3a494b77668fbdd1e4bd9
Sharewith-Lemon <zhangmeng_tom@163.com>
2020-09-04 16:41:42 +0800

    change file

--- Luntan/Luntan/run.py
+++
@@ -1,9 +0,0 @@
-from scrapy import cmdline
-
-
-cmdline.execute('scrapy crawl kaoyanbang'.split())
-cmdline.execute('scrapy crawl kaoyanbang_ban'.split())
-cmdline.execute('scrapy crawl kaoyanbang_ban02'.split())
-cmdline.execute('scrapy crawl kaoyanbang_ban03'.split())
-cmdline.execute('scrapy crawl kaoyanbang_ban04'.split())
-cmdline.execute('scrapy crawl kaoyanbang_ban05'.split())
